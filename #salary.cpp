d1ea4feef472788f4a7a9f56de49ce7328067fea
Themandme <ajbigsis@icloud.com>
2022-10-04 19:22:12 -0400

    .

--- #salary.cpp
+++ #salary.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-using namespace std;
+ using namespace std;
 
-int main()
-{
-	int salary; 
-    int part;
-    int full; // initializing variables
-	cout << "Enter your salary" << endl; // asking for salary
-	cin >> salary; // inputting salary
-    part = salary * 10; // part time salary
-    full = salary * 40;  //full salary
-    cout << "You make $" << part << " dollars for every 10 hours worked " << "and $" << full << " for every 40 hours" << endl;
-}
+ int main()
+ {
+ 	int salary; 
+     int part;
+     int full; // initializing variables
+ 	cout << "Enter your hourly pay" << endl; // asking for salary
+ 	cin >> salary; // inputting salary
+     part = salary * 10; // part time salary
+     full = salary * 40;  //full salary
+     cout << "You make $" << part << " dollars for every 10 hours worked " << "and $" << full << " for every 40 hours!" << endl;
+ }
