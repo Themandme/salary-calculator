cabd674c0acb456f23b85665032369da261a6aca
Themandme <ajbigsis@icloud.com>
2022-10-04 19:18:40 -0400

    Create #salary.cpp
    Added basic functionality.

---
+++ #salary.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int salary; 
+    int part;
+    int full; // initializing variables
+	cout << "Enter your salary" << endl; // asking for salary
+	cin >> salary; // inputting salary
+    part = salary * 10; // part time salary
+    full = salary * 40;  //full salary
+    cout << "You make $" << part << " dollars for every 10 hours worked " << "and $" << full << " for every 40 hours" << endl;
+}
