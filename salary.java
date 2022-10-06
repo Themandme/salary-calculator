abe9479608c51e09aaf116a7f7a4f1ea12af5fe1
Themandme <ajbigsis@icloud.com>
2022-10-06 13:30:23 -0400

    Update salary.java

--- salary.java
+++ salary.java
@@ -12,10 +12,11 @@ public class salary{
         tax_rate= tax_rate/100;
         System.out.println("You make : " + part + "$ For every 20 hours of work" );
         System.out.println("You make :" + full + "$ For every 40 hours of work");
-        double full_time_post_tax = full * tax_rate;
-        double part_time_post_tax = part * tax_rate;
-        System.out.println("You make :" + full_time_post_tax + "$ After taxes working full time");
+        double full_time_post_tax = full - (full * tax_rate);
+        double part_time_post_tax = part - (part * tax_rate);
         System.out.println("You make :" + part_time_post_tax + "$ After taxes working part time");
+        System.out.println("You make :" + full_time_post_tax + "$ After taxes working full time");
+        
     }
 
 }
