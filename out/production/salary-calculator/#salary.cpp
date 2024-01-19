3235d4a05af25023bc4bff4ad6a85e99324e40a7
themandme <ajbigsis@icloud.com>
2024-01-18 21:11:14 -0500

    Divided program into classes

---
+++ out/production/salary-calculator/#salary.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ using namespace std;
+
+ int main()
+ {
+ 	int salary; 
+     int part;
+     int full; // initializing variables
+ 	cout << "Enter your hourly pay" << endl; // asking for salary
+ 	cin >> salary; // inputting salary
+     part = salary * 10; // part time salary
+     full = salary * 40;  //full salary
+     cout << "You make $" << part << " dollars for every 10 hours worked " << "and $" << full << " for every 40 hours!" << endl; // part time and full time calculations.
+     cout << "Your yearly salary is $" << full * 52 << "!" << endl; // outputting yearly salary
+ }
