9255b5be6edddd30acebab16789d5c7989952ffc
themandme <ajbigsis@icloud.com>
2024-01-18 21:33:53 -0500

    Divided program into classes

--- salary.java
+++ salary.java
@@ -5,19 +5,20 @@ public class salary{
     public static void main(String[] args) {
         System.out.println("Enter your hourly pay:");
         double salary = console.nextInt();
-        double part = salary * 20;
-        double full = salary * 40;
-        System.out.println("What's the tax rate of your state by percentage?");
+        System.out.println("How many hours a week do you work?");
+        int clock = console.nextInt();
+        double pay = salary * clock;
+        System.out.println("You make : " + tax(pay) + "$ Per Paycheck After taxes");
 
-        System.out.println("You make : " + tax(part) + "$ Per Paycheck After taxes working part time");
-        System.out.println("You make : " + tax(full) + "$ Per Paycheck After taxes working full time");
         
     }
+
     public static double tax(double salary){
-        double tax_rate = console.nextDouble();
-        double taxedsalary = (salary * (tax_rate / 100));
-        double full = taxedsalary + salary;
-        return full;
+        System.out.println("What's the tax rate of your state by percentage?");
+        double tax_rate = (console.nextDouble());
+        double ends = (salary /tax_rate * 100);
+        return ends;
+
     }
 
 }
